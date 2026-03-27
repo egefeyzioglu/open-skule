ee959516f5e85689199e55e6094555f1d497f6cf
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2026-03-26 22:37:21 -0400

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import Link from "next/link";
-
-import { LatestPost } from "src/app/_components/post";
-import { auth } from "src/server/auth";
-import { api, HydrateClient } from "src/trpc/server";
-
-import NavBar from "src/app/_components/navbar"
+import NavBar from "src/app/_components/navbar";
+import { HydrateClient } from "src/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
-
   return (
     <HydrateClient>
-      <NavBar></NavBar>
+      <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           {/* <div className="flex flex-col items-center gap-2">
